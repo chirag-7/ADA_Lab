a7d714803e360b184c22b64d92c079fe89f2cf29
chirag-7 <64130161+chirag-7@users.noreply.github.com>
2022-07-19 22:51:51 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,39 +1,41 @@
-#include<stdio.h>
-#include<conio.h>
-#include<time.h>
-#include<windows.h>
-int a[20][20],reach[20],n;
-void dfs(int v){
-int i;
-reach[v]=1;
-for(i=1;i<=n;i++)
-if(a[v][i]&&!reach[i]){
-printf("\tn%d-->%d",v,i);
-dfs(i);
-}
+#include <stdio.h>
+#include <conio.h>
+int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
+
+void bfs(int v)
+{
+    for (i = 1; i <= n; i++)
+        if (a[v][i] && !visited[i])
+            q[++r] = i;
+    if (f <= r)
+    {
+        visited[q[f]] = 1;
+        bfs(q[f++]);
+    }
 }
 
-int main(){
-clock_t s,e;
-int i,j,count=0;
-printf("enter the no of vertices;\n");
-scanf("%d",&n);
-printf("enter the adjacency matrix:\n");
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-scanf("%d",&a[i][j]);
-s=clock();
-dfs(1);
-Sleep(400);
-e=clock();
-for(i=1;i<=n;i++)
-if(reach[i])
-count++;
-if(count==n)
-printf("\n graph connected");
-else
-printf("\n graph is disconnected");
-printf("\nthe time taken for sorting the elements where n:%d is %f",n,((double)(e-s))/CLK_TCK);
-return(0);
+void main()
+{
+    int v;
+
+    printf("\n Enter the number of vertices:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        q[i] = 0;
+        visited[i] = 0;
+    }
+    printf("\n Enter graph data in matrix form:\n");
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
+    printf("\n Enter the starting vertex:");
+    scanf("%d", &v);
+    bfs(v);
+    printf("\n The node which are reachable are:\n");
+    for (i = 1; i <= n; i++)
+        if (visited[i])
+            printf("%d\t", i);
+    getch();
 }
 
